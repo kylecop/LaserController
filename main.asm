eda4aad5ba0265aa1468e934bf2588915dd96eea
kylecop <43895134+kylecop@users.noreply.github.com>
2020-01-17 08:48:08 -0600

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,10 +1,10 @@
 .org 0x200 ; create 7SEG CODE TABLE at address 0x100 (word address, which will be byte address of 200)
-data1:.DB 'A', 'B', 0b01000011, 0b01000100, 0b01000101, 0b01000110, 0b01000111, 0b01001000, 0b01001001, 0b01001010, 0b01001011, 0b01001100, 0b01001101, 0b01001110, 0b01001111, 0b01010000, 0b01010001, 0b01010010
-;            A       B            C          D           E           F            G           H          I            J          K            L           M          N            O          P           Q           R
+data1:.DB 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'
+
 .org 0x300 ; create 7SEG CODE TABLE at address 0x100 (word address, which will be byte address of 200)
 data2:.DB 0b01010011, 0b01010100, 0b01010101, 0b01010110, 0b01010111, 0b01011000, 0b01011001, 0b01011010, 0B00110000, 0B00110001, 0B00110010, 0B00110011, 0B00110100, 0B00110101, 0B00110110, 0B00110111, 0B00111000, 0B00111001
 ;            s       t            u          v           W           X            Y           Z           0           1           2           3           4           5           6           7           8           9
-         
+
 .ORG 0X00
 JMP START
 .ORG 0X02
@@ -13,15 +13,16 @@ JMP INT0ROUTINE
 INT0ROUTINE:	
 //	LDI R16,'H'
 //	CALL DATAWRT
-	IN R16,PORTC
+	IN R17,PortC
 	CALL LOADZREGISTER1
 	CALL DATAWRT
+	CLR R17
 RETI
 
 LoadZRegister1:
 	ldi ZL, low(2*data1)
 	ldi ZH, high(2*data1)
-	add zl,r16 ; add the BCD  value to be converted to low byte of 7SEG CODE TABLE to create an offset numerically equivalent to BCD value 
+	add zl,r17 ; add the BCD  value to be converted to low byte of 7SEG CODE TABLE to create an offset numerically equivalent to BCD value 
 	lpm r16,z ; load z into r17 from program memory from7SEG CODE TABLE using modified z register as pointer
 ret
 
@@ -34,8 +35,10 @@ ret
 
 START:
 
-    LDI R16, 0x00 ; load 1's into R16
-	OUT DDRC, R16 ; output 1's to configure DDRB as "input" port
+INITIALIZE_KEYPAD_INPUT:
+    LDI R16, 0x00 ; load 0's into R16
+	OUT DDRC, R16 ; output 1's to configure DDRc as "input" port
+	OUT PORTC, R16 ; output 1's to configure DDRc as "input" port
 
 CONFIGURE_INTERRUPTS:
 	LDI R31,0X0A
@@ -83,6 +86,7 @@ INITIALIZE_LCD:
 	CALL DELAY_2ms
 	LDI R16,0X06
 	CALL CMNDWRT
+
 
 MAIN:
 	RJMP MAIN ; WE CAN STILL BE INTERRUPTED, BUT WE WILL RETURN HERE
